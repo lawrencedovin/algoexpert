6c5e9d8aee2c7a489df6a96f11f318a2dd693d4c
lawrencedovin <lawrence@qwikwire.com>
2020-08-25 22:44:20 -0400

    Added two number sum solution 2

---
+++ easy/two-number-sum-2/lib/script.js
@@ -0,0 +1,16 @@
+// O(n) time | O(n) space
+function twoNumberSum(array, targetSum) {
+    const nums = {};
+    for(const num of array) {
+        const potentialMatch = targetSum - num;
+        // y = targetSum - x;
+        if (potentialMatch in nums) {
+            return [potentialMatch, num];
+        } else {
+            nums[num] = true;
+        }
+    }
+    return [];
+  }
+
+  console.log(twoNumberSum([1,2,3,4,5,6], 4));
