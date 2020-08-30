063ffcc96bed8ff064388e9ee09103d7a9ecf61b
lawrencedovin <lawrence@qwikwire.com>
2020-08-29 20:28:42 -0400

    added closest value in BST example 2

---
+++ easy/closest-value-bst-2/lib/script.js
@@ -0,0 +1,16 @@
+// Average: O(log(n)) time | O(1) space
+// Worst: O(n) time | O(1) space 
+function findClosestValueInBst(tree, target) {
+	return findClosestValueInBstHelper(tree, target, Infinity);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+	let currentNode = tree;
+	while(currentNode !== None){
+		if (Math.abs(target - closest) > Math.abs(target - currentNode.values)) closest = currentNode.value;
+		if (target < currentNode.value) currentNode = currentNode.left;
+		else if (target > currentNode.value) currentNode = currentNode.right;
+		else break;
+	}
+	return closest;
+}
